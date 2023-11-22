c071a6a15cf072d5c1ea8745bfd7c8e6dee7523b
Peter Macdonald <macdonald.peter90@gmail.com>
2023-11-22 21:21:03 +0100

    working on tests
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- src/service/router.test.ts
+++ src/service/router.test.ts
@@ -1,12 +1,15 @@
 import { getVoidLogger } from '@backstage/backend-common';
 import express from 'express';
 import request from 'supertest';
-import axios from 'axios';
 import { createRouter } from './router';
 import { ConfigReader } from '@backstage/config';
+import fetch, { Response } from 'node-fetch';
 
-jest.mock('node-fetch');
-const mockedFetch = fetch as jest.Mocked<typeof axios>;
+jest.mock('node-fetch', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('node-fetch', () => jest.fn());
 
 describe('createRouter', () => {
   let app: express.Express;
@@ -41,110 +44,124 @@ describe('createRouter', () => {
     jest.resetAllMocks();
   });
 
-  describe('POST /entity-checker', () => {
-    it('returns data from OPA', async () => {
-      const mockResponse = { data: { result: 'mockResult' } };
-      mockedFetch.post.mockResolvedValue(mockResponse);
-
-      config.getOptionalString('opaClient.baseUrl');
-
-      const response = await request(app)
-        .post('/entity-checker')
-        .send({ input: 'entityMetadata' });
+  describe('GET /opa/health', () => {
+    it('returns ok', async () => {
+      const response = await request(app).get('/opa/health');
 
       expect(response.status).toEqual(200);
-      expect(response.body).toEqual(mockResponse.data.result);
+      expect(response.body).toEqual({ status: 'ok' });
     });
+  });
 
-    it('handles entity metadata route error', async () => {
-      mockedFetch.post.mockRejectedValue(new Error());
+  describe('POST /entity-checker', () => {
+    it('returns data from OPA', async () => {
+      const mockResponse = { result: 'mockResult' };
+      const response = new Response(JSON.stringify(mockResponse), {
+        status: 200,
+      });
 
-      const response = await request(app)
+      (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValue(
+        Promise.resolve(response),
+      );
+
+      const res = await request(app)
         .post('/entity-checker')
         .send({ input: 'entityMetadata' });
 
-      expect(response.status).toEqual(500);
-      expect(response.body.message).toContain(
-        `An error occurred trying to send entity metadata to OPA`,
-      );
+      expect(res.status).toEqual(200);
+      expect(res.body).toEqual(mockResponse);
     });
   });
 
-  describe('POST /opa-permissions', () => {
-    it('returns 400 if OPA URL is not set or missing', async () => {
-      const noBaseUrlConfig = new ConfigReader({
-        opaClient: {
-          policies: {
-            entityChecker: {
-              package: 'entitymeta_policy',
-            },
-            rbac: {
-              package: 'rbac_policy',
-            },
-          },
-        },
-      });
+  //   it('handles entity metadata route error', async () => {
+  //     mockedFetch.post.mockRejectedValue(new Error());
 
-      const router = await createRouter({
-        logger: getVoidLogger(),
-        config: noBaseUrlConfig,
-      });
+  //     const response = await request(app)
+  //       .post('/entity-checker')
+  //       .send({ input: 'entityMetadata' });
 
-      const noBaseUrl = express().use(router);
+  //     expect(response.status).toEqual(500);
+  //     expect(response.body.message).toContain(
+  //       `An error occurred trying to send entity metadata to OPA`,
+  //     );
+  //   });
+  // });
 
-      const response = await request(noBaseUrl)
-        .post('/opa-permissions')
-        .send({ input: 'policyInput' });
+  // describe('POST /opa-permissions', () => {
+  //   it('returns 400 if OPA URL is not set or missing', async () => {
+  //     const noBaseUrlConfig = new ConfigReader({
+  //       opaClient: {
+  //         policies: {
+  //           entityChecker: {
+  //             package: 'entitymeta_policy',
+  //           },
+  //           rbac: {
+  //             package: 'rbac_policy',
+  //           },
+  //         },
+  //       },
+  //     });
 
-      expect(response.status).toEqual(400);
-    });
+  //     const router = await createRouter({
+  //       logger: getVoidLogger(),
+  //       config: noBaseUrlConfig,
+  //     });
 
-    it('returns 400 if OPA RBAC package is not set or missing', async () => {
-      const noRbacPackageConfig = new ConfigReader({
-        opaClient: {
-          baseUrl: 'http://localhost',
-          policies: {
-            entityChecker: {
-              package: 'entitymeta_policy',
-            },
-          },
-        },
-      });
+  //     const noBaseUrl = express().use(router);
 
-      const router = await createRouter({
-        logger: getVoidLogger(),
-        config: noRbacPackageConfig,
-      });
+  //     const response = await request(noBaseUrl)
+  //       .post('/opa-permissions')
+  //       .send({ input: 'policyInput' });
 
-      const noRbacPackage = express().use(router);
+  //     expect(response.status).toEqual(400);
+  //   });
 
-      const response = await request(noRbacPackage)
-        .post('/opa-permissions')
-        .send({ input: 'policyInput' });
+  //   it('returns 400 if OPA RBAC package is not set or missing', async () => {
+  //     const noRbacPackageConfig = new ConfigReader({
+  //       opaClient: {
+  //         baseUrl: 'http://localhost',
+  //         policies: {
+  //           entityChecker: {
+  //             package: 'entitymeta_policy',
+  //           },
+  //         },
+  //       },
+  //     });
 
-      expect(response.status).toEqual(400);
-      expect(response.body.message).toEqual(
-        'OPA RBAC package not set or missing!',
-      );
-    });
+  //     const router = await createRouter({
+  //       logger: getVoidLogger(),
+  //       config: noRbacPackageConfig,
+  //     });
 
-    it('returns 400 if policy input is missing', async () => {
-      const response = await request(app).post('/opa-permissions').send({});
+  //     const noRbacPackage = express().use(router);
 
-      expect(response.status).toEqual(400);
-      expect(response.body.message).toEqual('The policy input is missing!');
-    });
+  //     const response = await request(noRbacPackage)
+  //       .post('/opa-permissions')
+  //       .send({ input: 'policyInput' });
 
-    it('returns 500 if an error occurs when sending policy input to OPA', async () => {
-      mockedFetch.post.mockRejectedValue(new Error());
-      const response = await request(app)
-        .post('/opa-permissions')
-        .send({ policyInput: 'policydata' });
+  //     expect(response.status).toEqual(400);
+  //     expect(response.body.message).toEqual(
+  //       'OPA RBAC package not set or missing!',
+  //     );
+  //   });
 
-      expect(response.status).toEqual(500);
-      expect(response.body.message).toEqual(
-        'An error occurred trying to send policy input to OPA',
-      );
-    });
-  });
+  //   it('returns 400 if policy input is missing', async () => {
+  //     const response = await request(app).post('/opa-permissions').send({});
+
+  //     expect(response.status).toEqual(400);
+  //     expect(response.body.message).toEqual('The policy input is missing!');
+  //   });
+
+  //   it('returns 500 if an error occurs when sending policy input to OPA', async () => {
+  //     mockedFetch.post.mockRejectedValue(new Error());
+  //     const response = await request(app)
+  //       .post('/opa-permissions')
+  //       .send({ policyInput: 'policydata' });
+
+  //     expect(response.status).toEqual(500);
+  //     expect(response.body.message).toEqual(
+  //       'An error occurred trying to send policy input to OPA',
+  //     );
+  //   });
+  // });
 });
